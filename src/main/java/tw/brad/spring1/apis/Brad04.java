f009bf1977d3593886234e40d49b602046b3be85
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-25 11:56:30 +0800

    07251156

--- src/main/java/tw/brad/spring1/apis/Brad04.java
+++ src/main/java/tw/brad/spring1/apis/Brad04.java
@@ -1,5 +1,8 @@
 package tw.brad.spring1.apis;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,43 @@ public class Brad04 {
 		int r = x + y;
 		System.out.printf("%d + %d + %d\n", x, y, r);
 		return r+"";
+	}
+	
+	@RequestMapping("/m3")
+	public void m3(@RequestBody User user) {
+		System.out.println(user.getName());
+		System.out.println(user.getGender());
+		System.out.println(user.getAge());
+	}
+	
+	@RequestMapping("/m4/{id}/{name}")
+	public void m4(
+			@PathVariable String name,
+			@PathVariable Integer id) {
+		System.out.printf("%d : %s\n", id, name);
+	}
+	
+	@RequestMapping("/m5/{id}/{name}")
+	public void m5(
+			@PathVariable String name,
+			@PathVariable Integer id,
+			@RequestParam(required = false, defaultValue = "0") String x, 
+			@RequestParam(required = false, defaultValue = "0") String y,
+			@RequestBody User user			
+			) {
+		System.out.printf("%d : %s : %s : %s\n", id, name, x, y);
+		System.out.println(user.getName());
+		System.out.println(user.getGender());
+		System.out.println(user.getAge());		
+	}
+	
+	@RequestMapping("/m6")
+	public void m6(
+			@RequestHeader String x, 
+			@RequestHeader Integer y,
+			@RequestHeader(name = "Content-Type") String contentType
+			) {
+		System.out.printf("%s : %d : %s\n", x, y, contentType);
 	}
 	
 	
