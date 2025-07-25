47208d9c0001027ba138edb3a579b5bb64d0510c
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-25 15:32:56 +0800

    07251532

--- src/main/java/tw/brad/spring1/apis/Brad07.java
+++ src/main/java/tw/brad/spring1/apis/Brad07.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +45,53 @@ public class Brad07 {
 		
 		return response;
 	}
+	
+	@DeleteMapping("/member/{id}")
+	public Response delMember(@PathVariable Integer id) {
+		String sql = """
+				DELETE FROM member 
+				WHERE id = :id
+				""";
+		Map<String,Integer> params = new HashMap<>();
+		params.put("id", id);
+		
+		jdbc.update(sql, params);
+		
+		response.setError(0);
+		response.setMesg("Success");
+		return response;
+	}
+	
+	@PutMapping("/member")
+	public Response updateMember(@RequestBody Member member) {
+		String sql = """
+				UPDATE member
+				SET account = :account, name = :name 
+				WHERE id = :id
+				""";
+		Map<String,Object> params = new HashMap<>();
+		params.put("id", member.getId());
+		params.put("account", member.getAccount());
+		params.put("name", member.getName());
+		
+		int n = jdbc.update(sql, params);
+		if (n > 0) {
+			response.setError(0);
+			response.setMesg("Success");
+		}else {
+			response.setError(1);
+			response.setMesg("Update Failure");
+		}
+		
+		return response;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
